7fa512306c772b2d850e0a0d065002240ae436f1
Víctor <systemdeveloper868@gmail.com>
2020-07-25 20:26:21 +0200

    js mod css

--- functions.php
+++ functions.php
@@ -4,13 +4,14 @@ function Tema1_agregar_css_js() {
   wp_enqueue_style( 'style', get_stylesheet_uri());
   wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '1.1', 'all');
   wp_enqueue_script( 'popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array ( 'jquery' ), '1.16', true);
-  wp_enqueue_script( 'bootstrap-js','https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', array('popper'), '4.5',true);
-   
+  wp_enqueue_script( 'bootstrap-js','https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', array('popper'), '4.5',true);  
+  /* Mis Javascript personalizados */
+  wp_enqueue_script( 'main-js',get_template_directory_uri() . '/js/main.js', array('bootstrap-js'), '1.0',true);  
   }
   add_action( 'wp_enqueue_scripts', 'Tema1_agregar_css_js' );
 
-  /* Añade en la configuración donde Post la opcion de Imagenes destacadas*/
-  if ( function_exists( 'add_theme_support' ) ) {
+/* Añade en la configuración donde Post la opcion de Imagenes destacadas*/
+if ( function_exists( 'add_theme_support' ) ) {
     add_theme_support( 'post-thumbnails' );
     set_post_thumbnail_size( 150, 150, true ); // default Featured Image dimensions (cropped)
  
@@ -32,3 +33,4 @@ function Tema1_widgets(){
   ));
 }
 add_action('widgets_init','Tema1_widgets');
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-/*ya vere que meto*/
+/* Manupulando Css y Boostrap con JavaScript */
+const formularioBuscar = document.querySelector('#searchform #s');
+formularioBuscar.className += ' form-control mb-2';
+
+/* Manupulando Css y Boostrap con JavaScript */
+const btnBuscar = document.querySelector('#searchsubmit ');
+btnBuscar.className += ' btn btn-primary btn-block mb-2';
